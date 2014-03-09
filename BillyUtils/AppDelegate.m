35e7ca398465e42ce5d7db9932a469c22d9e677a
Dan Dela Rosa <dandelarosa@gmail.com>
2014-03-09 18:50:47 -0400

    Initial Commit

---
+++ BillyUtils/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  BillyUtils
+//
+//  Created by Dan Dela Rosa on 3/9/14.
+//  Copyright (c) 2014 MRD Engineering. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ BillyUtils/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  BillyUtils
+//
+//  Created by Dan Dela Rosa on 3/9/14.
+//  Copyright (c) 2014 MRD Engineering. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
